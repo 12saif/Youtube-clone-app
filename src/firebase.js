b883fc02ec30d2335db160960fda03e21423ffb5
md saif <mohdsaif9170613062@gmail.com>
2023-04-05 01:59:21 +0530

    Create Header Components

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCh4zjGVQzkFVxo74ULeCYvbRatgSJt6qo",
+  authDomain: "clone-53957.firebaseapp.com",
+  projectId: "clone-53957",
+  storageBucket: "clone-53957.appspot.com",
+  messagingSenderId: "429143283061",
+  appId: "1:429143283061:web:16a0039b8eb778809f2f3c",
+  measurementId: "G-S6NX3Y5QPK",
+};
