bfdacaef0bedbd47ba08af27331c15cb7fa1fa3d
md saif <mohdsaif9170613062@gmail.com>
2023-04-10 16:56:50 +0530

    Sidebar Icon section Home, Trending, Subscription

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,10 +1,17 @@
 import React from "react";
+import SidebarRow from "../SidebarRow/SidebarRow";
 import "./Sidebar.css";
+
+import HomeSharpIcon  from '@mui/icons-material/HomeSharp';
+import WhatshotSharpIcon from "@mui/icons-material/WhatshotSharp";
+import SubscriptionsSharpIcon from '@mui/icons-material/SubscriptionsSharp';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>This is a Sidebar.</h1>
+      <SidebarRow Icon={HomeSharpIcon} title="Home" />
+      <SidebarRow Icon={WhatshotSharpIcon} title="Trending" />
+      <SidebarRow Icon={SubscriptionsSharpIcon} title="Subscription" />
     </div>
   );
 }
